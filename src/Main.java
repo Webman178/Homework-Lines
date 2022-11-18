da2548b385f526cf1fa1fdd4aa1d22b9e10f334a
Иван <triger080@mail.ru>
2022-11-18 17:08:18 +0300

    ДЗ

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,26 @@
+import java.util.Locale;
+
 public class Main {
     public static void main(String[] args) {
+        //Задание 1
+        System.out.println("Заадание 1");
+        String firstName = "Ivan ";
+        String middleName = "Ivanovich";
+        String lastName = "Ivanov ";
+        String fullName = lastName + firstName + middleName;
+        System.out.println("ФИО сотрудника - " + fullName);
+
+        //Задание 2
+        System.out.println("Заадание 2");
+        fullName = fullName.toUpperCase(Locale.ROOT);
+        System.out.println("Данные ФИО сотрудника для заполнения отчета — " + fullName);
+
+        //Задание 3
+        System.out.println("Заадание 3");
+        String fullName1 = "Иванов Семён Семёнович";
+        fullName1 = fullName1.replace("ё", "е");
+        System.out.println("Данные ФИО сотрудника — " + fullName1);
+
 
     }
 }
